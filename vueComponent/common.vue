6c72722231cb12286c52cab7839dda499d31dcc0
UnlimitedGalaxy <1464345037@qq.com>
2017-06-08 18:15:40 +0800

    stylus component  and js tools update 6.8 galaxy

---
+++ vueComponent/common.vue
@@ -0,0 +1,32 @@
+<template>
+    <div >
+        <div @click="$emit('click')"></div>
+        <div :style="{backgroundImage: 'url('+indexData.supplier_logo+')'}"></div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        props: {
+        	value: String
+        },
+        data() {
+        	return {
+        		dynamicVal: this.value
+            }
+        },
+        watch: {
+        	value(val) {
+        		this.dynamicVal = val
+            },
+            dynamicVal(val) {
+        		this.$emit('input', val)
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    @import "../../../common/stylus/mixin.styl"
+
+</style>
